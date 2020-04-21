48b4a7e37377797e830025c403da61a51c3f3c66
Pellennen <pelle_9z@gotmail.com>
2020-04-21 22:19:05 +0200

    second-commit

--- src/screens/ViewNotes.js
+++ src/screens/ViewNotes.js
@@ -1,27 +1,53 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Text, FAB } from "react-native-paper";
-import Header from '../components/Header'
-import React, { useState } from 'react'
+import { StyleSheet, View, FlatList } from 'react-native'
+import { Text, FAB, List } from 'react-native-paper'
+import Header from "../components/Header";
+import React, { useState } from "react";
 
 function ViewNotes({ navigation }) {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = note => {
+    note.id = notes.length + 1;
+    setNotes([...notes, note]);
+  };
+
   return (
     <>
-    <Header titleText="Simple note taker"/>
-    <View style={styles.container}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>You do not have any notes</Text>
+      <Header titleText='Simple Note Taker' />
+      <View style={styles.container}>
+        {notes.length === 0 ? (
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>You do not have any notes</Text>
+          </View>
+        ) : (
+          <FlatList
+            data={notes}
+            renderItem={({ item }) => (
+              <List.Item
+                title={item.noteTitle}
+                description={item.noteValue}
+                descriptionNumberOfLines={1}
+                titleStyle={styles.listTitle}
+              />
+            )}
+            keyExtractor={item => item.id.toString()}
+          />
+        )}
+        <FAB
+          style={styles.fab}
+          small
+          icon='plus'
+          label='Add new note'
+          // add a second parameter object
+          onPress={() =>
+            navigation.navigate('AddNotes', {
+              addNote
+            })
+          }
+        />
       </View>
-      <FAB
-        style={styles.fab}
-        small
-        icon="plus"
-        label="Add new note"
-        onPress={() => navigation.navigate("AddNotes")}
-      ></FAB>
-    </View>
     </>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -36,15 +62,18 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     flex: 1
   },
+  listTitle: {
+    fontSize: 20
+  },
   title: {
     fontSize: 20
   },
   fab: {
-   position: 'absolute',
-   margin: 20,
-   right: 0,
-   bottom: 10
- }
+    position: "absolute",
+    margin: 20,
+    right: 0,
+    bottom: 10
+  }
 });
 
 export default ViewNotes;
